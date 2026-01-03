95f1595d3424b592a663161cf9b4c91c5ccdc73c
Taricol <mozumder9122023@gmail.com>
2026-01-03 23:25:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import NavBar from './NavBar/NavBar'
+
+function App() {
+  
+  return (
+    <>
+     
+     <header>
+      <NavBar></NavBar>
+       <DaisyNav></DaisyNav>
+     </header>
+     <main>
+      
+     </main>
+
+      
+      
+    </>
+  )
+}
+
+export default App
